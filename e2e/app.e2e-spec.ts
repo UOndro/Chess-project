de23773a6270bace26cf866535ba86611cb8191a
Angular CLI <angular-cli@angular.io>
2017-05-21 11:06:35 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { ChessProjectPage } from './app.po';
+
+describe('chess-project App', () => {
+  let page: ChessProjectPage;
+
+  beforeEach(() => {
+    page = new ChessProjectPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
